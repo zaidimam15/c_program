594a4d25532b0a3f7a9f01bb18690cfa2419a792
zaidimam <zaidimam15@gmail.com>
2023-12-03 16:40:39 +0530

    findmarks_avg_per

---
+++ marksof5subject/findthemarks_avg_per.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+    float eng, phy, chem, math, comp;
+    float total, average, percentage;
+
+    /* Input marks of all five subjects */
+    printf("Enter marks of five subjects: \n");
+    scanf("%f%f%f%f%f", &eng, &phy, &chem, &math, &comp);
+
+    /* Calculate total, average and percentage */
+    total = eng + phy + chem + math + comp;
+    average = total / 5.0;
+    percentage = (total / 500.0) * 100;
+
+    /* Print all results */
+    printf("Total marks = %.2f\n", total);
+    printf("Average marks = %.2f\n", average);
+    printf("Percentage = %.2f", percentage);
+
+    getch ();
+}
